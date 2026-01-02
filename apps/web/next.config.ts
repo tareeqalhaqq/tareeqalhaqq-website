2b94f78e784ba2bac9e739f8d0854defd68ebc84
Mabc365 <manhajassalafiyyah3@gmail.com>
2026-01-02 16:29:12 -0500

    fixing stuff

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,8 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  trailingSlash: true,
   transpilePackages: [
     "@radix-ui/react-dialog",
     "@radix-ui/react-dropdown-menu",
