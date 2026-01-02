a8ce29573acc07f3da85427309b6b9e9689ace41
tareeqalhaqq <manhajassalafiyyah3@gmail.com>
2026-01-01 23:04:46 -0500

    Replace supabase-js with SSR client

--- src/utils/supabase/clients.ts
+++ src/utils/supabase/clients.ts
@@ -1,10 +1,10 @@
-import { createClient as createSupabaseClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const createClient = () =>
-  createSupabaseClient(
+  createBrowserClient(
     supabaseUrl!,
     supabaseKey!,
   );
