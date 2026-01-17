86d655ce929380b24dab0b5cf0c8b33ade8a6bac
Mabc365 <tareeqalhaqq3@gmail.com>
2026-01-16 22:53:51 -0500

    Remove tareeqalhaqq-mobile app and related files
    Deleted the entire tareeqalhaqq-mobile application, including all source code, configuration, components, services, and dependencies. This removes the app from the repository.

--- apps/tareeqalhaqq-mobile/app/(tabs)/index.tsx
+++
@@ -1,5 +0,0 @@
-import { DashboardScreen } from "@/features/dashboard/DashboardScreen";
-
-export default function DashboardRoute() {
-  return <DashboardScreen />;
-}
--- apps/tareeqalhaqq-mobile/app/(tabs)/notes.tsx
+++
@@ -1,5 +0,0 @@
-import { NotesScreen } from "@/features/notes/NotesScreen";
-
-export default function NotesRoute() {
-  return <NotesScreen />;
-}
